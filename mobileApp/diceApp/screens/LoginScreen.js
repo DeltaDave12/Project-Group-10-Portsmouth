3189bf32e657c3b7b77c9b6fa9b76c7279ce46ad
Valentin THOMAS <91453935+Croniums@users.noreply.github.com>
2023-02-24 20:35:21 +0000

    Version Marie login/Join us

--- mobileApp/diceApp/screens/LoginScreen.js
+++ mobileApp/diceApp/screens/LoginScreen.js
@@ -5,31 +5,30 @@ import Ionicons from '@expo/vector-icons/Ionicons';
 
 export default function LoginScreen({navigation}){
   return (
-    <ImageBackground source={require('./img/dicesplash0322.jpg')} resizeMode="cover" style={styles.imageBk}>
+    <ImageBackground source={require('./img/portsmouthabouttop.jpg')} resizeMode="cover" style={styles.imageBk}>
         <LinearGradient
           // Background Linear Gradient
           colors={['rgba(0,0,0,0.50)', 'transparent']}
           style={styles.background}
         />
       <View style={styles.container}>
-        <Image source={require('./img/LOGO_Black.png')} style={styles.image}/>
+        <Image source={require('./img/LOGO_White.fw.png')} style={styles.image}/>
         <View style={styles.login}>
           <View style={styles.buttons}>
             <Pressable style={styles.button1}><Text style={styles.textButton1}>Login</Text></Pressable>
-            <Pressable onPress={() => navigation.navigate('SignIn')} style={styles.button2}><Text style={styles.button2Text}>Sign in</Text></Pressable>
+            <Pressable onPress={() => navigation.navigate('SignIn')} style={styles.button2}><Text style={styles.button2Text}>Join us</Text></Pressable>
           </View>
-          <Text style={styles.Title}>Login</Text>
           <View style={styles.Icons}>
             <Ionicons name="logo-google" size={40} color="ghostwhite"/>
             <Ionicons name="logo-facebook" size={40} color="ghostwhite"/>
           </View>
           <View style={styles.inputs}>
-            <TextInput placeholder='Email' style={styles.input} placeholderTextColor="#512F07"></TextInput>
+            <TextInput placeholder='Mail' style={styles.input} placeholderTextColor="#512F07"></TextInput>
           </View>
           <View style={styles.inputs}>
             <TextInput placeholder='Password' style={styles.input} placeholderTextColor="#512F07"></TextInput>
           </View>
-          <Pressable onPress={() => navigation.navigate('Home')} style={styles.LoginButton}><Text>Login</Text></Pressable>
+          <Pressable onPress={() => navigation.navigate('Home')} style={styles.LoginButton}><Text style={styles.LoginButtonText} >Login</Text></Pressable>
         </View>
       </View>
     </ImageBackground>
@@ -38,7 +37,7 @@ export default function LoginScreen({navigation}){
 
 const styles = StyleSheet.create({
     image: {
-      left: 0,
+      left: '31%',
       width: 130,
       height: 100,
     },
@@ -70,9 +69,10 @@ const styles = StyleSheet.create({
     button2:  {
       alignItems: 'center',
       justifyContent: 'center',
-      width: 75,
-      height: 50,
+      width: '55%',
+      height: '43%',
       backgroundColor: '#fcb900',
+      borderRadius: 30,
     },
     button2Text: {
       color: 'white',
@@ -96,6 +96,9 @@ const styles = StyleSheet.create({
       width: 175,
       height: 50,
       backgroundColor: '#fcb900',
+    },
+    LoginButtonText: {
+      color: 'ghostwhite',
     },
     input: {
       backgroundColor: 'white',
--- mobileApp/diceApp/screens/SignInScreen.js
+++ mobileApp/diceApp/screens/SignInScreen.js
@@ -5,36 +5,35 @@ import Ionicons from '@expo/vector-icons/Ionicons';
 
 export default function SignInScreen({navigation}){
   return (
-    <ImageBackground source={require('./img/dicesplash0322.jpg')} resizeMode="cover" style={styles.imageBk}>
+    <ImageBackground source={require('./img/portsmouthabouttop.jpg')} resizeMode="cover" style={styles.imageBk}>
       <LinearGradient
         // Background Linear Gradient
         colors={['rgba(0,0,0,0.50)', 'transparent']}
         style={styles.background}
       />
     <View style={styles.container}>
-      <Image source={require('./img/LOGO_Black.png')} style={styles.image}/>
+      <Image source={require('./img/LOGO_White.fw.png')} style={styles.image}/>
       <View style={styles.login}>
         <View style={styles.buttons}>
           <Pressable onPress={() => navigation.navigate('Login')} style={styles.button1}><Text style={styles.textButton1}>Login</Text></Pressable>
-          <Pressable style={styles.button2}><Text style={styles.textButton2} >Sign in</Text></Pressable>
+          <Pressable style={styles.button2}><Text style={styles.textButton2} >Join us</Text></Pressable>
         </View>
-        <Text style={styles.Title}>Sign In</Text>
         <View style={styles.Icons}>
           <Ionicons name="logo-google" size={40} color="ghostwhite"/>
           <Ionicons name="logo-facebook" size={40} color="ghostwhite"/>
         </View>
         <View style={styles.inputs}>
-          <TextInput placeholder='Name' style={styles.input} placeholderTextColor="#512F07"></TextInput>
           <TextInput placeholder='Surname' style={styles.input} placeholderTextColor="#512F07"></TextInput>
+          <TextInput placeholder='Name' style={styles.input} placeholderTextColor="#512F07"></TextInput>
         </View>
         <View style={styles.inputs}>
-          <TextInput placeholder='Email' style={styles.input} placeholderTextColor="#512F07"></TextInput>
+          <TextInput placeholder='Mail' style={styles.input} placeholderTextColor="#512F07"></TextInput>
           <TextInput placeholder='Password' style={styles.input} placeholderTextColor="#512F07"></TextInput>
         </View>
         <View style={styles.inputs1}>
           <TextInput placeholder='Birthday Date' style={styles.input} placeholderTextColor="#512F07"></TextInput>
         </View>
-        <Pressable onPress={() => navigation.navigate('Home')} style={styles.SignInButton}><Text>SignIn</Text></Pressable>
+        <Pressable onPress={() => navigation.navigate('Home')} style={styles.SignInButton}><Text style={styles.SignInButtonText}>Create</Text></Pressable>
       </View>
       
     </View>
@@ -44,9 +43,10 @@ export default function SignInScreen({navigation}){
 
 const styles = StyleSheet.create({
     image: {
-      left: 0,
-      width: 130,
-      height: 100,
+      top: '1%',
+      left: '35%',
+      width: '30%',
+      height: '10%',
     },
     container: {
       marginVertical: '10%',
@@ -56,23 +56,24 @@ const styles = StyleSheet.create({
       alignItems: 'center',
     },
     buttons: {
-      marginTop: 100,
-      width: 170,
+      marginTop: '24%',
+      width: '45%',
       flexDirection: 'row',
       justifyContent: 'space-between',
     },
     button1:  {
       alignItems: 'center',
       justifyContent: 'center',
-      width: 75,
-      height: 50,
+      width: '55%',
+      height: '38%',
+      borderRadius: 30,
       backgroundColor: '#fcb900',
     },
     button2:  {
       alignItems: 'center',
       justifyContent: 'center',
-      width: 75,
-      height: 50,
+      width: '50%',
+      height: '38%',
       borderBottomWidth: 3,
       borderColor: '#fcb900',
     },
@@ -84,14 +85,14 @@ const styles = StyleSheet.create({
     },
     Icons: {
       marginTop: 15,
-      width: 125,
+      width: '35%',
       flexDirection: 'row',
       justifyContent: 'space-between',
     },
     Title: {
       color: 'ghostwhite',
       fontSize: 35,
-      marginTop: 50,
+      marginTop: '0%',
     },
     SignInButton: {
       color: 'ghostwhite',
@@ -99,29 +100,32 @@ const styles = StyleSheet.create({
       marginTop: 50,
       alignItems: 'center',
       justifyContent: 'center',
-      width: 175,
-      height: 50,
+      width: '40%',
+      height: '7%',
       backgroundColor: '#fcb900',
+    },
+    SignInButtonText: {
+      color: 'ghostwhite'
     },
     input: {
       backgroundColor: 'white',
       paddingLeft: 15,
-      height: 35,
+      height: '125%',
       width: 135,
       borderWidth: 0,
       borderColor: 'lightgray',
       borderRadius: 15,
-      fontSize: 12,
+      fontSize: 14,
     },
     inputs: {
       marginTop: 35,
-      width: 325,
+      width: '80%',
       justifyContent: 'space-between',
       flexDirection: 'row',
     },
     inputs1: {
       marginTop: 50,
-      width: 200,
+      width: '100%',
       justifyContent: 'center',
       flexDirection: 'row',
     },
