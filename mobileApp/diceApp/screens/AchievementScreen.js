660e52be747e6470ac5ec8da62080089fbb1c99f
Gafrunkles <david.rossig12@gmail.com>
2023-03-13 20:40:52 +0000

    Update AchievementScreen.js

--- mobileApp/diceApp/screens/AchievementScreen.js
+++ mobileApp/diceApp/screens/AchievementScreen.js
@@ -1,6 +1,5 @@
 import { StyleSheet, Text, View, Button, Image, ScrollView, TextInput, Pressable, Alert, TouchableOpacity, Modal, TouchableOpacityComponent } from 'react-native'
 import React, { useState, useEffect } from 'react'
- 
 
 export default function AchievementScreen({navigation}) {
    
